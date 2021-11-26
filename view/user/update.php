80ce06d87f9500790b0be0c94fb88527df7b3ee3
Nancy Siong <nancy.siong@etu.umontpellier.fr>
2021-11-26 10:30:05 +0100

    creation champ de verification mdp

--- view/user/update.php
+++ view/user/update.php
@@ -8,39 +8,52 @@
       <input type='hidden' name='controller' value='user'>
     </p>
     <?php
-    echo'<p>
+
+    echo
+    '<p>
     <label for="login_id">Login</label> :
-    <input type="text" placeholder="nouveau Login" name="newlogin" id="newlogin_id"/>
+    <input type="text" placeholder="exemple@gmail.com" name="newlogin" id="newlogin_id"/>
     </p> 
+    <p>
+      <label for="surname_id">Nom</label> : 
+      <input type="text" placeholder="nouveau nom" name="newsurname" id="newsurname_id" />
+    </p>
     <p>
       <label for="name_id">Prénom</label> :
       <input type="text" placeholder="nouveau prénom" name="newname" id="newname_id" />
     </p>
     <p>
       <label for="password_id">Mot de passe</label> : 
-      <input type="text" placeholder="nouveau mot de passe" name="newpassword" id="newpassword_id" />
+      <input type="text" placeholder="Nouveau mot de passe" name="newpassword" id="newpassword_id" />
     </p>
     <p>
-      <label for="surname_id">Nom</label> : 
-      <input type="text" placeholder="nouveau nom" name="newsurname" id="newsurname_id" />
+      <label for="password_id">Ressaisissez le mot passe</label> : 
+      <input type="text" placeholder="Nouveau mot de passe" name="newpassword" id="newpassword_id" 
+      />
     </p>
 
+
     <p>
-      <input type="hidden" name="login" id="login_id" value=' . 
-      (isset($u) ? rawurlencode($u->getLogin()) : "") . ' . />
+      <input type="hidden" name="login" id="login_id" 
+      value=' . (isset($u) ? rawurlencode($u->getLogin()) : "") . ' . />
     </p>
     <p>
-      <input type="hidden" name="name" id="name_id" value=' .
-      (isset($u) ? rawurlencode($u->getName()) : "") . ' . />
+      <input type="hidden" name="name" id="name_id" 
+      value=' . (isset($u) ? rawurlencode($u->getName()) : "") . ' . />
     </p>
     <p>
-      <input type="hidden" name="surname" id="surname_id" value=' .
-      (isset($u) ? rawurlencode($u->getSurname()) : "") . ' . />
+      <input type="hidden" name="surname" id="surname_id" 
+      value=' . (isset($u) ? rawurlencode($u->getSurname()) : "") . ' . />
     </p>
     <p>
-      <input type="hidden" name="password" id="password_id" value=' . 
-      (isset($u) ? rawurlencode($u->getPassword()) : "") . ' . />
-    </p>'
+      <input type="hidden" name="password" id="password_id" 
+      value=' . (isset($u) ? rawurlencode($u->getPassword()) : "") . ' . />
+    </p>
+    <p>
+      <input type="hidden" name="password" id="password_id" 
+      value=' . (isset($u) ? rawurlencode($u->getPassword()) : "") . ' . />
+    </p>
+    '
     ?>
     <p>
       <input type="submit" value="Envoyer" />
